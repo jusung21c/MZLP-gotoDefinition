50ae2469fd271905de7e57f7fe8e86051d5d682e
jusung21c <jusung21c@gmail.com>
2017-05-17 21:02:06 +0900

    FR
    FR

--- spec/mzlp-gotodefinition-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import MzlpGotodefinitionView from '../lib/mzlp-gotodefinition-view';
-
-describe('MzlpGotodefinitionView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
